80f6575cf5edfb1993d0328b0ff0d7406c1df7bd
Mohit18023 <mohitchoudhary1054@gmail.com>
2024-07-15 14:36:40 +0530

    type error resolved

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,49 +1,73 @@
-"use client"
-import MeetingTypeList from '@/components/MeetingTypeList';
-import React, { useEffect, useState } from 'react'
-import { useGetCalls } from '@/hooks/useGetCalls';
+"use client";
+import MeetingTypeList from "@/components/MeetingTypeList";
+import React, { useEffect, useState } from "react";
+import { useGetCalls } from "@/hooks/useGetCalls";
+import { Call } from "@stream-io/video-client"
 
 const Home = () => {
-  const {  upcomingCalls, isLoading } =
-    useGetCalls();
-    const [nextCall,setNextCall]=useState();
-      useEffect(() => {
+  const { upcomingCalls, isLoading } = useGetCalls();
+  const [nextCall, setNextCall] = useState<Call | undefined>(undefined);
+  useEffect(() => {
     if (upcomingCalls && upcomingCalls.length > 0) {
       const earliestUpcomingCall = upcomingCalls.reduce((earliest, current) => {
-        return new Date(current.state.startsAt) < new Date(earliest.state.startsAt) ? current : earliest;
+        const currentStart =
+          current.state?.startsAt ?? new Date().toISOString();
+        const earliestStart =
+          earliest.state?.startsAt ?? new Date().toISOString();
+
+        return new Date(currentStart) < new Date(earliestStart)
+          ? current
+          : earliest;
       }, upcomingCalls[0]);
+
       setNextCall(earliestUpcomingCall);
     }
   }, [upcomingCalls]);
-  const now=new Date();
-  const time=now.toLocaleTimeString('india',{hour:'2-digit',minute:'2-digit'});
-  const date=(new Intl.DateTimeFormat('india',{dateStyle:'full'})).format(now);
 
-    const getNextCallDateTime = (startsAt) => {
+
+  const now = new Date();
+  const time = now.toLocaleTimeString("india", {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+  const date = new Intl.DateTimeFormat("india", { dateStyle: "full" }).format(
+    now
+  );
+
+  const getNextCallDateTime = (startsAt:string|undefined) => {
+    if (!startsAt) return "No upcoming meetings";
     const startDate = new Date(startsAt);
-    const callTime = startDate.toLocaleTimeString('india', { hour: '2-digit', minute: '2-digit' });
-    const callDate = new Intl.DateTimeFormat('india', { dateStyle: 'full' }).format(startDate);
+    const callTime = startDate.toLocaleTimeString("india", {
+      hour: "2-digit",
+      minute: "2-digit",
+    });
+    const callDate = new Intl.DateTimeFormat("india", {
+      dateStyle: "full",
+    }).format(startDate);
     return `${callDate} at ${callTime}`;
   };
   return (
     <section className="flex size-full flex-col gap-10 text-white">
-     <div className='h-[300px] w-full rounded-[20px] bg-hero bg-cover'>
-      <div className='flex flex-col justify-between h-full max-md:px-5 max-md:py-8 lg:p-11'>
-        <h2 className='glassmorphism  rounded py-2 text-center text-base font-normal'> {nextCall ? `Upcoming meeting on ${getNextCallDateTime(nextCall.state.startsAt)}` : "No Upcoming meetings"}</h2>
-        <div className='flex flex-col gap-2'>
-      <h1 className='text-4xl font-extrabold lg:text-7xl'>
-        {time}
-      </h1>
-      <p className='text-lg font-medium text-sky-1 lg:text-2xl'>{date}</p>
-     </div>
-     
+      <div className="h-[300px] w-full rounded-[20px] bg-hero bg-cover">
+        <div className="flex flex-col justify-between h-full max-md:px-5 max-md:py-8 lg:p-11">
+          <h2 className="glassmorphism  rounded py-2 text-center text-base font-normal">
+            {" "}
+            {nextCall
+              ? getNextCallDateTime(
+                  nextCall.state?.startsAt as string | undefined
+                )
+              : "No upcoming meetings"}
+          </h2>
+          <div className="flex flex-col gap-2">
+            <h1 className="text-4xl font-extrabold lg:text-7xl">{time}</h1>
+            <p className="text-lg font-medium text-sky-1 lg:text-2xl">{date}</p>
+          </div>
+        </div>
       </div>
-    
-     </div>
 
-     <MeetingTypeList/>
+      <MeetingTypeList />
     </section>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -1,6 +1,14 @@
-import { cn } from '@/lib/utils';
-import { CallControls, CallingState, CallParticipantsList, CallStatsButton, PaginatedGridLayout, SpeakerLayout, useCallStateHooks } from '@stream-io/video-react-sdk';
-import React, { useEffect, useState } from 'react';
+import { cn } from "@/lib/utils";
+import {
+  CallControls,
+  CallingState,
+  CallParticipantsList,
+  CallStatsButton,
+  PaginatedGridLayout,
+  SpeakerLayout,
+  useCallStateHooks,
+} from "@stream-io/video-react-sdk";
+import React, { useEffect, useState } from "react";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -8,76 +16,93 @@ import {
   DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
-import { LayoutList, Users } from 'lucide-react';
-import { useRouter, useSearchParams } from 'next/navigation';
-import EndCallButton from './EndCallButton';
-import Loader from './Loader';
+} from "@/components/ui/dropdown-menu";
+import { LayoutList, Users } from "lucide-react";
+import { useRouter, useSearchParams } from "next/navigation";
+import EndCallButton from "./EndCallButton";
+import Loader from "./Loader";
 
-type CallLayoutType='grid'|'speaker-left'|'speaker-right'
+
+type CallLayoutType = "grid" | "speaker-left" | "speaker-right";
 const MeetingRoom = () => {
-    const searchParams=useSearchParams();
-    const isPersonalRoom=!!searchParams.get('personal');
-    const [layout, setLayout] = useState<CallLayoutType>('speaker-left');
-    const [showParticipants,setShowParticipants]=useState(false);
-    const {useCallCallingState}=useCallStateHooks();
-    const callingState=useCallCallingState();
-    const router=useRouter();
-    if(callingState!==CallingState.JOINED) return <Loader/>
-    const CallLayout=()=>{
-        switch(layout){
-            case 'grid':
-                return <PaginatedGridLayout/>
-                break;
-            case 'speaker-right':
-                return <SpeakerLayout participantsBarPosition="left"/>
-                break;
-            default:
-               return <SpeakerLayout participantsBarPosition="right"/>
-                break; 
-        }
+  const searchParams = useSearchParams();
+  const isPersonalRoom = !!searchParams.get("personal");
+  const [layout, setLayout] = useState<CallLayoutType>("speaker-left");
+  const [showParticipants, setShowParticipants] = useState(false);
+  const { useCallCallingState } = useCallStateHooks();
+  const callingState = useCallCallingState();
+  const router = useRouter();
+  if (callingState !== CallingState.JOINED) return <Loader />;
+  const CallLayout = () => {
+    switch (layout) {
+      case "grid":
+        return <PaginatedGridLayout />;
+        break;
+      case "speaker-right":
+        return <SpeakerLayout participantsBarPosition="left" />;
+        break;
+      default:
+        return <SpeakerLayout participantsBarPosition="right" />;
+        break;
     }
+  };
   return (
-    <section className='relative h-screen w-full overflow-hidden pt-4 text-white'>
-        <div className='relative flex size-full items-center justify-center'>
-            <div className='flex size-full max-w-[1000px] items-center'>
-             <CallLayout/>
-            </div>
-            <div className={cn('h-[calc(100vh-86px)] hidden ml-2',{'show-block':showParticipants})}>
-                <CallParticipantsList onClose={()=>setShowParticipants(false)}/>
-            </div>
+    <section className="relative h-screen w-full overflow-hidden pt-4 text-white">
+      <div className="relative flex size-full items-center justify-center">
+        <div className="flex size-full max-w-[1000px] items-center">
+          <CallLayout />
         </div>
-        <div className='fixed bottom-0 flex w-full items-center justify-center gap-5 flex-wrap'>
-            <CallControls onLeave={()=>{router.push('/')}}/>
-            <DropdownMenu>
-                <div className='flex items-center '>
-  <DropdownMenuTrigger className='cursor-pointer rounded-2xl  bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]'>
-    <LayoutList size={20} className='text-white'/>
-  </DropdownMenuTrigger>
-                </div>
-  <DropdownMenuContent className='border-dark-1 bg-dark-1 text-white'>
-     {['Grid','Speaker-left','Speaker-right'].map((item,index)=>(
-        <div key={index}>
-        <DropdownMenuItem className='cursor-pointer' onClick={()=>{setLayout(item.toLowerCase() as CallLayoutType)}}>
-            {item}
-        </DropdownMenuItem>
-        <DropdownMenuSeparator className='border-dark-1' />
+        <div
+          className={cn("h-[calc(100vh-86px)] hidden ml-2", {
+            "show-block": showParticipants,
+          })}
+        >
+          <CallParticipantsList onClose={() => setShowParticipants(false)} />
         </div>
-     ) )}
-
-    
-  </DropdownMenuContent>
-</DropdownMenu>
-<CallStatsButton/>
-<button onClick={()=>setShowParticipants((prev)=>!prev)}>
-<div className='cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]' >
-<Users size={20} className='text-white'/>
-</div>
-</button>
-{!isPersonalRoom && <EndCallButton/>}
-        </div>
+      </div>
+      <div className="fixed bottom-0 flex w-full items-center justify-center gap-5 flex-wrap">
+        <CallControls
+          onLeave={() => {
+            router.push("/");
+          }}
+        />
+        <DropdownMenu>
+          <div className="flex items-center ">
+            <DropdownMenuTrigger className="cursor-pointer rounded-2xl  bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]">
+              <LayoutList size={20} className="text-white" />
+            </DropdownMenuTrigger>
+          </div>
+          <DropdownMenuContent className="border-dark-1 bg-dark-1 text-white">
+            {["Grid", "Speaker-left", "Speaker-right"].map((item, index) => (
+              <div key={index}>
+                <DropdownMenuItem
+                  className="cursor-pointer"
+                  onClick={() => {
+                    setLayout(item.toLowerCase() as CallLayoutType);
+                  }}
+                >
+                  {item}
+                </DropdownMenuItem>
+                <DropdownMenuSeparator className="border-dark-1" />
+              </div>
+            ))}
+          </DropdownMenuContent>
+        </DropdownMenu>
+        <CallStatsButton />
+        <button
+          //type="button"
+          onClick={() => setShowParticipants((prev) => !prev)}
+        >
+          {}
+          <div className="cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]">
+            <Users size={20} className="text-white" />
+          </div>
+        </button>
+        {!isPersonalRoom && <EndCallButton />}
+        
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default MeetingRoom
+export default MeetingRoom;
