1a19a405b50f48e1a3df3c2db82886c2ebb29ac3
Mohit18023 <mohitchoudhary1054@gmail.com>
2024-07-19 16:25:04 +0530

    error resolved

--- Providers/StreamClientProvider.tsx
+++ Providers/StreamClientProvider.tsx
@@ -1,45 +1,44 @@
 "use client";
 
+import { tokenProvider } from "@/actions/stream.actions";
+import Loader from "@/components/Loader";
+import { useUser } from "@clerk/nextjs";
+import { StreamVideo, StreamVideoClient } from "@stream-io/video-react-sdk";
+import { ReactNode, useEffect, useState } from "react";
 
-import { tokenProvider } from '@/actions/stream.actions';
-import Loader from '@/components/Loader';
-import { useUser } from '@clerk/nextjs';
-import {
-    StreamVideo,
-    StreamVideoClient
-  } from '@stream-io/video-react-sdk';
-import { error } from 'console';
-import { ReactNode, useEffect, useState } from 'react';
-  
-  const apiKey = process.env.NEXT_PUBLIC_STREAM_API_KEY;
-  
- const StreamVideoProvider = ({children}:{children:ReactNode}) => {
-  const [videoClient, setVideoClient] = useState<StreamVideoClient>()
-  const {user,isLoaded}=useUser();
-  useEffect(()=>{
-         if(!isLoaded||!user) return;
-        if(!apiKey) throw new Error("stream api key missing");
-        const client=new StreamVideoClient({
-          apiKey,
-          user:{
-            id:user?.id,
-            name:user?.username||user?.id,
-            image:user?.imageUrl,
-          },
-          tokenProvider,
-            options: {
-      wsURL: wss://chat.stream-io-api.com // Use the secure WebSocket URL
-    }
-        })
-        setVideoClient(client);
-  },[user,isLoaded]);
+const StreamVideoProvider = ({ children }: { children: ReactNode }) => {
+  const [videoClient, setVideoClient] = useState<StreamVideoClient | undefined>(
+    undefined
+  );
+  const { user, isLoaded } = useUser();
 
-  if(!videoClient) return <Loader/>
-    return (
-      <StreamVideo client={videoClient}>
-       {children}
-      </StreamVideo>
-    );
-  };
+  useEffect(() => {
+    const initializeClient = async () => {
+      if (!isLoaded || !user) return;
+      const apiKey = process.env.NEXT_PUBLIC_STREAM_API_KEY;
+      if (!apiKey) throw new Error("Stream API key missing");
 
-  export default StreamVideoProvider;
+      const token = await tokenProvider();
+
+       const client = new StreamVideoClient({
+         apiKey,
+         token,
+         user: {
+           id: user.id,
+           name: user.username || user.id,
+           image: user.imageUrl,
+         },
+       });
+
+      setVideoClient(client);
+    };
+
+    initializeClient();
+  }, [user, isLoaded]);
+
+  if (!videoClient) return <Loader />;
+
+  return <StreamVideo client={videoClient}>{children}</StreamVideo>;
+};
+
+export default StreamVideoProvider;
